a5b7dda3b44dae72b6434929ec1bc67cac0a5de7
hendriku <hendriku@users.noreply.github.com>
2025-07-22 14:31:13 +0000

    deploy: bricksandbraces/arcade-designsystem@07f5aac045e319e3de581d1195d73dc5cdaea204

---
+++ assets/Dropdown.stories--ceaJ6kF.js
@@ -0,0 +1,22 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{b as p}from"./index.esm-D1niAXLM.js";import"./index-D4lIrffr.js";import"./index-BQQLSK9g.js";import"./index-DsJinFGm.js";const b={component:p,title:"Components/Dropdown",decorators:[c=>t.jsx("div",{className:"p-xlg pt-[256px]",children:t.jsx(c,{})})]},e={args:{content:"This is a Dropdown",title:"Click me",size:"medium"},argTypes:{size:{description:"Size of the dropdown",control:{type:"select"},options:["small","medium","large"]}}},o={args:{content:"Dropdown at the bottom",placement:"bottom",children:t.jsx("button",{children:"Hover me"})}};var n,r,s;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    content: "This is a Dropdown",
+    title: "Click me",
+    size: "medium"
+  },
+  argTypes: {
+    size: {
+      description: "Size of the dropdown",
+      control: {
+        type: "select"
+      },
+      options: ["small", "medium", "large"]
+    }
+  }
+}`,...(s=(r=e.parameters)==null?void 0:r.docs)==null?void 0:s.source}}};var a,m,i;o.parameters={...o.parameters,docs:{...(a=o.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    content: "Dropdown at the bottom",
+    placement: "bottom",
+    children: <button>Hover me</button>
+  }
+}`,...(i=(m=o.parameters)==null?void 0:m.docs)==null?void 0:i.source}}};const x=["Default","BottomPlacement"];export{o as BottomPlacement,e as Default,x as __namedExportsOrder,b as default};
