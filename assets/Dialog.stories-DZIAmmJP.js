e4474ecdf263ca1bd6533e34c7996133dbbeb4a2
hendriku <hendriku@users.noreply.github.com>
2025-08-05 19:32:05 +0000

    deploy: bricksandbraces/arcade-designsystem@6e354c22e0403ca90b381b5c660f20c23a630773

---
+++ assets/Dialog.stories-DZIAmmJP.js
@@ -0,0 +1,49 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{C as f,N as O,z as L,T as v,A,a as M}from"./index.esm-jBsp1lfi.js";import{r as D}from"./index-D4lIrffr.js";import"./index-BQQLSK9g.js";import"./index-DsJinFGm.js";const T={title:"Components/Dialog",args:{title:"Dialog Title",subtitle:"Subtitle or Description",primaryButton:{label:"Button",onClick:()=>alert("Primary action clicked")},secondaryButton:{label:"Button",onClick:()=>alert("Secondary action clicked")},onClose:()=>alert("Dialog closed"),description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",children:t.jsx("div",{className:"border-border-brand p-lg rounded-md border-[1px] border-dashed",children:t.jsx("span",{className:"text-text-brand typography-body-small",children:"Children"})})}},r={name:"DialogHeader",render:e=>t.jsx(f,{title:e.title,subtitle:e.subtitle})},a={name:"DialogBody",render:e=>t.jsx(O,{description:e.description,children:e.children})},s={name:"DialogFooter",render:e=>t.jsx(L,{primaryButton:e.primaryButton,secondaryButton:e.secondaryButton})},n={args:{size:"md"},argTypes:{size:{control:{type:"select"},options:["sm","md","lg","xlg"]}},render:e=>{const[d,o]=D.useState(!1);return t.jsxs(t.Fragment,{children:[t.jsx(v,{onClick:()=>o(!0),label:"Open Modal"}),t.jsx(A,{...e,open:d,onClose:()=>o(!1),children:e.children})]})}},i={render:e=>{const[d,o]=D.useState(!1);return t.jsxs(t.Fragment,{children:[t.jsx(v,{onClick:()=>o(!0),label:"Open Sheet"}),t.jsx(M,{...e,open:d,onClose:()=>o(!1),children:e.children})]})}};var l,m,u;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  name: "DialogHeader",
+  render: (args: any) => {
+    return <DialogHeader title={args.title} subtitle={args.subtitle} />;
+  }
+}`,...(u=(m=r.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var c,p,g;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  name: "DialogBody",
+  render: (args: any) => {
+    return <DialogBody description={args.description}>{args.children}</DialogBody>;
+  }
+}`,...(g=(p=a.parameters)==null?void 0:p.docs)==null?void 0:g.source}}};var y,b,h;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  name: "DialogFooter",
+  render: (args: any) => {
+    return <DialogFooter primaryButton={args.primaryButton} secondaryButton={args.secondaryButton} />;
+  }
+}`,...(h=(b=s.parameters)==null?void 0:b.docs)==null?void 0:h.source}}};var S,k,x;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  args: {
+    size: "md"
+  },
+  argTypes: {
+    size: {
+      control: {
+        type: "select"
+      },
+      options: ["sm", "md", "lg", "xlg"]
+    }
+  },
+  render: (args: ModalProps) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [open, setOpen] = useState(false);
+    return <>
+        <Button onClick={() => setOpen(true)} label="Open Modal" />
+        <DialogModal {...args as ModalProps} open={open} onClose={() => setOpen(false)}>
+          {args.children}
+        </DialogModal>
+      </>;
+  }
+}`,...(x=(k=n.parameters)==null?void 0:k.docs)==null?void 0:x.source}}};var C,B,j;i.parameters={...i.parameters,docs:{...(C=i.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  render: (args: SheetProps) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [open, setOpen] = useState(false);
+    return <>
+        <Button onClick={() => setOpen(true)} label="Open Sheet" />
+        <DialogSheet {...args as SheetProps} open={open} onClose={() => setOpen(false)}>
+          {args.children}
+        </DialogSheet>
+      </>;
+  }
+}`,...(j=(B=i.parameters)==null?void 0:B.docs)==null?void 0:j.source}}};const N=["HeaderComponent","BodyComponent","FooterComponent","Modal","Sheet"];export{a as BodyComponent,s as FooterComponent,r as HeaderComponent,n as Modal,i as Sheet,N as __namedExportsOrder,T as default};
--- assets/NotificationClose.stories-BftdRCQs.js
+++
@@ -1,5 +0,0 @@
-import{j as a}from"./jsx-runtime-D_zvdyIk.js";import{s as e}from"./index.esm-Cn46w_Lm.js";import{a as i}from"./index-B-lxVbXh.js";import"./index-D4lIrffr.js";import"./index-BQQLSK9g.js";import"./index-DsJinFGm.js";import"./v4-CtRu48qb.js";const u={title:"Components/NotificationClose",component:e},o=({...n})=>a.jsx(e,{...n});o.args={onClick:i("onClick")};o.__docgenInfo={description:"",methods:[],displayName:"Playground"};o.__docgenInfo={description:"",methods:[],displayName:"Playground"};var r,t,s;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`({
-  ...args
-}: React.ComponentProps<typeof NotificationClose> & Record<string, any>) => {
-  return <NotificationClose {...args} />;
-}`,...(s=(t=o.parameters)==null?void 0:t.docs)==null?void 0:s.source}}};const y=["Playground"];export{o as Playground,y as __namedExportsOrder,u as default};
