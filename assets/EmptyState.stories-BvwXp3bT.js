f4088b3cde010d93b5d372f96353f3a7d7487627
hendriku <hendriku@users.noreply.github.com>
2025-08-10 16:26:33 +0000

    deploy: bricksandbraces/arcade-designsystem@1ee34e02a7fb37b8e38dce8ab2a9843646bb472d

--- assets/EmptyState.stories-BvwXp3bT.js
+++
@@ -1,8 +0,0 @@
-import{j as o}from"./jsx-runtime-D_zvdyIk.js";import{E as i}from"./Input-BF_BM5ZX.js";import"./index-D4lIrffr.js";import"./index-C7yw4mlf.js";import"./index-BQQLSK9g.js";import"./index-DsJinFGm.js";import"./borders-N_bRvfGw.js";import"./colors-DFCrPdrr.js";const E={title:"Components/EmptyState",component:i,argTypes:{orientation:{control:{type:"select"},options:["horizontal","vertical"]}}},t=({text:s,...a})=>o.jsx("div",{children:o.jsx(i,{...a,children:s})});t.args={description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore.",title:"No search items found.",orientation:"horizontal"};t.__docgenInfo={description:"",methods:[],displayName:"Playground"};t.__docgenInfo={description:"",methods:[],displayName:"Playground"};var e,r,n;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`({
-  text,
-  ...args
-}: React.ComponentProps<typeof EmptyState> & Record<string, any>) => {
-  return <div>
-      <EmptyState {...args}>{text}</EmptyState>
-    </div>;
-}`,...(n=(r=t.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const x=["Playground"];export{t as Playground,x as __namedExportsOrder,E as default};
