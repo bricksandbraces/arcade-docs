ee8c516ede0c6560cedd4cfe089b4495e27aac94
hendriku <hendriku@users.noreply.github.com>
2025-09-03 15:12:22 +0000

    deploy: bricksandbraces/arcade-designsystem@720950000ef626c185b346b6786c87c5abcd2a6f

--- assets/NotificationClose.stories-DVR8Km0u.js
+++
@@ -1,5 +0,0 @@
-import{j as s}from"./jsx-runtime-D_zvdyIk.js";import{q as e}from"./TextArea-V7Z0Yj8U.js";import"./index-D4lIrffr.js";import{a}from"./index-B-lxVbXh.js";import"./index-BQQLSK9g.js";import"./index-DsJinFGm.js";import"./borders-N_bRvfGw.js";import"./colors-DFCrPdrr.js";import"./index-C7yw4mlf.js";import"./v4-CtRu48qb.js";const N={title:"Components/Notification/NotificationClose",component:e},o=({...n})=>s.jsx(e,{...n});o.args={onClick:a("onClick")};o.__docgenInfo={description:"",methods:[],displayName:"Playground"};o.__docgenInfo={description:"",methods:[],displayName:"Playground"};var t,r,i;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`({
-  ...args
-}: React.ComponentProps<typeof NotificationClose> & Record<string, any>) => {
-  return <NotificationClose {...args} />;
-}`,...(i=(r=o.parameters)==null?void 0:r.docs)==null?void 0:i.source}}};const _=["Playground"];export{o as Playground,_ as __namedExportsOrder,N as default};
