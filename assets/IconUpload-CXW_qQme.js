1dd298af774cff18cc490152037100cb985fdb50
hendriku <hendriku@users.noreply.github.com>
2025-09-09 16:14:49 +0000

    deploy: bricksandbraces/arcade-designsystem@f7d392ef901a96008df6333e0f48b31a4796654e

---
+++ assets/IconUpload-CXW_qQme.js
@@ -0,0 +1,21 @@
+import{c as a}from"./createReactComponent-DYmUEkme.js";/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=[["path",{d:"M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1",key:"svg-0"}],["path",{d:"M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z",key:"svg-1"}],["path",{d:"M16 5l3 3",key:"svg-2"}]],l=a("outline","edit","Edit",t);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["path",{d:"M12 3a7 7 0 0 1 7 7v4l-3 -3",key:"svg-0"}],["path",{d:"M22 11l-3 3",key:"svg-1"}],["path",{d:"M8 15.5l-5 -3l5 -3l5 3v5.5l-5 3z",key:"svg-2"}],["path",{d:"M3 12.5v5.5l5 3",key:"svg-3"}],["path",{d:"M8 15.545l5 -3.03",key:"svg-4"}]],v=a("outline","rotate-3d","Rotate3d",o);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const e=[["path",{d:"M4 7l16 0",key:"svg-0"}],["path",{d:"M10 11l0 6",key:"svg-1"}],["path",{d:"M14 11l0 6",key:"svg-2"}],["path",{d:"M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12",key:"svg-3"}],["path",{d:"M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3",key:"svg-4"}]],h=a("outline","trash","Trash",e);/**
+ * @license @tabler/icons-react v3.34.1 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=[["path",{d:"M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2",key:"svg-0"}],["path",{d:"M7 9l5 -5l5 5",key:"svg-1"}],["path",{d:"M12 4l0 12",key:"svg-2"}]],n=a("outline","upload","Upload",s);export{l as I,v as a,n as b,h as c};
---
+++ assets/Lottie.stories-BoT1Jg5e.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{z as o}from"./index-ChaIqHV-.js";import"./index-0yr9KlQE.js";const m={title:"Essentials/Lottie"},e=()=>t.jsxs(t.Fragment,{children:["https://developers.lottiefiles.com/docs/tools/dotlottie-js/theming/",t.jsx("br",{}),"describes how to theme",t.jsx(o,{className:"max-h-[200px] max-w-[200px]",height:200,src:"/lottie/b-default.lottie",loop:!0,autoplay:!0}),t.jsx(o,{className:"max-h-[200px] max-w-[200px]",height:200,src:"/lottie/c-default.lottie",loop:!0,autoplay:!0}),t.jsx(o,{className:"max-h-[200px] max-w-[200px]",height:200,src:"/lottie/w-default.lottie",loop:!0,autoplay:!0})]});e.__docgenInfo={description:"",methods:[],displayName:"Lottie"};e.__docgenInfo={description:"",methods:[],displayName:"Lottie"};var s,a,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`() => {
+  return <>
+      https://developers.lottiefiles.com/docs/tools/dotlottie-js/theming/
+      <br />
+      describes how to theme
+      <DotLottieReact className="max-h-[200px] max-w-[200px]" height={200} src="/lottie/b-default.lottie" loop autoplay />
+      <DotLottieReact className="max-h-[200px] max-w-[200px]" height={200} src="/lottie/c-default.lottie" loop autoplay />
+      <DotLottieReact className="max-h-[200px] max-w-[200px]" height={200} src="/lottie/w-default.lottie" loop autoplay />
+    </>;
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const c=["Lottie"];export{e as Lottie,c as __namedExportsOrder,m as default};
