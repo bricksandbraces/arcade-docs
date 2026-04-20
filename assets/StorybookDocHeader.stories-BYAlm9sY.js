94f0135524fa0c1cc8ced38842dd62656fe1710a
hendriku <hendriku@users.noreply.github.com>
2026-04-20 09:48:26 +0000

    deploy: bricksandbraces/arcade-designsystem@cbdc4c27bbf98fb40ca3f66318d49478f7ea5302

--- assets/StorybookDocHeader.stories-BYAlm9sY.js
+++
@@ -1,5 +0,0 @@
-import{bA as a}from"./ContextMenuList-w4l5Bqjq.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-CY-HDqYb.js";import"./index-A6zbrgSY.js";import"./index-DWvd7WUe.js";import"./index-BDYMFBcC.js";import"./index-ZRCHFLd-.js";import"./sizes-BpUsT7VU.js";import"./colors-CoOIoG7W.js";const g={title:"Storybook/StorybookDocHeader",component:a,argTypes:{title:{control:"text"}}},o={args:{title:"Button"}};var t,r,e;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    title: "Button"
-  }
-}`,...(e=(r=o.parameters)==null?void 0:r.docs)==null?void 0:e.source}}};const y=["Playground"];export{o as Playground,y as __namedExportsOrder,g as default};
