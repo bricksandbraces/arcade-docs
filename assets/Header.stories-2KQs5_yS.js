c39d5a4c2d21d5b86067fa4276753323a79ae19a
hendriku <hendriku@users.noreply.github.com>
2026-04-20 07:56:08 +0000

    deploy: bricksandbraces/arcade-designsystem@6b11618850a403ae6b338b0af2abe752cdc9b18a

---
+++ assets/Header.stories-2KQs5_yS.js
@@ -0,0 +1,185 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{ai as M,aj as O,h as n,_ as i,ak as t,al as L,am as I,an as S,ao as s}from"./ContextMenuList-AIp8CmXM.js";import"./index-CY-HDqYb.js";import"./index-A6zbrgSY.js";import"./index-DWvd7WUe.js";import"./index-BDYMFBcC.js";import"./index-ZRCHFLd-.js";import"./sizes-BpUsT7VU.js";import"./colors-CoOIoG7W.js";const q={title:"WIP Components/Header/Header",component:O,argTypes:{variant:{control:"inline-radio",options:M},border:{control:"boolean"}},parameters:{layout:"fullscreen"}},r={args:{variant:"desktop",border:!1,children:e.jsxs(e.Fragment,{children:[e.jsxs("div",{className:"gap-xs flex items-center",children:[e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"←"}),e.jsx("div",{className:"gap-xs px-lg flex h-[40px] items-center rounded-md",children:e.jsx("span",{className:"typography-headline-06 text-text",children:"Title"})}),e.jsx(i,{orientation:"vertical"}),e.jsx(t,{children:"Link"}),e.jsx(t,{children:"Link"}),e.jsx(t,{children:"Link"}),e.jsx(i,{orientation:"vertical"}),e.jsx(t,{children:"Link"}),e.jsxs(L,{children:[e.jsx(I,{children:"Link"}),e.jsxs(S,{children:[e.jsx(s,{children:"Option 1"}),e.jsx(s,{children:"Option 2"}),e.jsx(s,{children:"Option 3"})]})]}),e.jsx(t,{children:"Link"})]}),e.jsxs("div",{className:"gap-xs flex items-center",children:[e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"⚙️"}),e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"🔔"}),e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"☰"})]})]})}},a={args:{variant:"desktop",border:!1,children:e.jsxs(e.Fragment,{children:[e.jsxs("div",{className:"gap-xs flex items-center",children:[e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"←"}),e.jsx("div",{className:"gap-xs px-lg flex h-[40px] items-center rounded-md",children:e.jsx("span",{className:"typography-headline-06 text-text",children:"Dashboard"})}),e.jsx(i,{orientation:"vertical"}),e.jsx(t,{children:"Overview"}),e.jsx(t,{selected:!0,children:"Analytics"}),e.jsx(t,{children:"Reports"}),e.jsx(i,{orientation:"vertical"}),e.jsx(t,{children:"Settings"}),e.jsxs(L,{children:[e.jsx(I,{children:"More"}),e.jsxs(S,{children:[e.jsx(s,{children:"Export Data"}),e.jsx(s,{children:"Import Data"}),e.jsx(s,{children:"Backup"})]})]}),e.jsx(t,{children:"Help"})]}),e.jsxs("div",{className:"gap-xs flex items-center",children:[e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"⚙️"}),e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"🔔"}),e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"☰"})]})]})}},d={args:{variant:"mobile",border:!1,children:e.jsxs(e.Fragment,{children:[e.jsxs("div",{className:"flex items-center",children:[e.jsx("div",{className:"gap-sm p-lg flex h-auto w-[40px] items-center justify-center",children:e.jsx("div",{className:"bg-text h-[16px] w-[18px] rounded-xs"})}),e.jsx(i,{orientation:"vertical"}),e.jsx("div",{className:"gap-xs px-lg flex h-[40px] items-center rounded-md",children:e.jsx("span",{className:"typography-headline-06 text-text",children:"Title"})})]}),e.jsxs("div",{className:"gap-xs flex",children:[e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"⚙️"}),e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"🔔"}),e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"☰"})]})]})},parameters:{viewport:{defaultViewport:"mobile1"}}},o={args:{variant:"desktop",border:!0,children:e.jsxs(e.Fragment,{children:[e.jsxs("div",{className:"gap-xs flex items-center",children:[e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"←"}),e.jsx("div",{className:"gap-xs px-lg flex h-[40px] items-center rounded-md",children:e.jsx("span",{className:"typography-headline-06 text-text",children:"Project Name"})}),e.jsx(i,{orientation:"vertical"}),e.jsx(t,{children:"Files"}),e.jsx(t,{children:"Issues"}),e.jsx(t,{selected:!0,children:"Pull Requests"}),e.jsx(t,{children:"Actions"})]}),e.jsxs("div",{className:"gap-xs flex items-center",children:[e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"⚙️"}),e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"🔔"})]})]})}},c={args:{variant:"desktop",border:!1,children:e.jsxs(e.Fragment,{children:[e.jsx("div",{className:"gap-xs flex items-center",children:e.jsx("div",{className:"gap-xs px-lg flex h-[40px] items-center rounded-md",children:e.jsx("span",{className:"typography-headline-06 text-text",children:"Simple Header"})})}),e.jsxs("div",{className:"gap-xs flex items-center",children:[e.jsx(t,{children:"Login"}),e.jsx(n,{kind:"primary",size:"medium",children:"Sign Up"})]})]})}},l={args:{variant:"mobile",border:!0,children:e.jsxs(e.Fragment,{children:[e.jsx("div",{className:"flex items-center",children:e.jsx("div",{className:"gap-sm p-lg flex h-auto w-[40px] items-center justify-center",children:e.jsx("div",{className:"bg-text h-[16px] w-[18px] rounded-xs"})})}),e.jsx(n,{kind:"ghost",type:"icon",size:"medium",children:"☰"})]})},parameters:{viewport:{defaultViewport:"mobile1"}}};var p,m,x;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    variant: "desktop",
+    border: false,
+    children: <>
+        <div className="gap-xs flex items-center">
+          <Button kind="ghost" type="icon" size="medium">
+            ←
+          </Button>
+          <div className="gap-xs px-lg flex h-[40px] items-center rounded-md">
+            <span className="typography-headline-06 text-text">Title</span>
+          </div>
+          <Divider orientation="vertical" />
+          <HeaderButton>Link</HeaderButton>
+          <HeaderButton>Link</HeaderButton>
+          <HeaderButton>Link</HeaderButton>
+          <Divider orientation="vertical" />
+          <HeaderButton>Link</HeaderButton>
+          <HeaderDropdown>
+            <HeaderDropdownButton>Link</HeaderDropdownButton>
+            <HeaderDropdownList>
+              <HeaderDropdownListItem>Option 1</HeaderDropdownListItem>
+              <HeaderDropdownListItem>Option 2</HeaderDropdownListItem>
+              <HeaderDropdownListItem>Option 3</HeaderDropdownListItem>
+            </HeaderDropdownList>
+          </HeaderDropdown>
+          <HeaderButton>Link</HeaderButton>
+        </div>
+        <div className="gap-xs flex items-center">
+          <Button kind="ghost" type="icon" size="medium">
+            ⚙️
+          </Button>
+          <Button kind="ghost" type="icon" size="medium">
+            🔔
+          </Button>
+          <Button kind="ghost" type="icon" size="medium">
+            ☰
+          </Button>
+        </div>
+      </>
+  }
+}`,...(x=(m=r.parameters)==null?void 0:m.docs)==null?void 0:x.source}}};var u,h,g;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    variant: "desktop",
+    border: false,
+    children: <>
+        <div className="gap-xs flex items-center">
+          <Button kind="ghost" type="icon" size="medium">
+            ←
+          </Button>
+          <div className="gap-xs px-lg flex h-[40px] items-center rounded-md">
+            <span className="typography-headline-06 text-text">Dashboard</span>
+          </div>
+          <Divider orientation="vertical" />
+          <HeaderButton>Overview</HeaderButton>
+          <HeaderButton selected>Analytics</HeaderButton>
+          <HeaderButton>Reports</HeaderButton>
+          <Divider orientation="vertical" />
+          <HeaderButton>Settings</HeaderButton>
+          <HeaderDropdown>
+            <HeaderDropdownButton>More</HeaderDropdownButton>
+            <HeaderDropdownList>
+              <HeaderDropdownListItem>Export Data</HeaderDropdownListItem>
+              <HeaderDropdownListItem>Import Data</HeaderDropdownListItem>
+              <HeaderDropdownListItem>Backup</HeaderDropdownListItem>
+            </HeaderDropdownList>
+          </HeaderDropdown>
+          <HeaderButton>Help</HeaderButton>
+        </div>
+        <div className="gap-xs flex items-center">
+          <Button kind="ghost" type="icon" size="medium">
+            ⚙️
+          </Button>
+          <Button kind="ghost" type="icon" size="medium">
+            🔔
+          </Button>
+          <Button kind="ghost" type="icon" size="medium">
+            ☰
+          </Button>
+        </div>
+      </>
+  }
+}`,...(g=(h=a.parameters)==null?void 0:h.docs)==null?void 0:g.source}}};var v,j,B;d.parameters={...d.parameters,docs:{...(v=d.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    variant: "mobile",
+    border: false,
+    children: <>
+        <div className="flex items-center">
+          <div className="gap-sm p-lg flex h-auto w-[40px] items-center justify-center">
+            <div className="bg-text h-[16px] w-[18px] rounded-xs"></div>
+          </div>
+          <Divider orientation="vertical" />
+          <div className="gap-xs px-lg flex h-[40px] items-center rounded-md">
+            <span className="typography-headline-06 text-text">Title</span>
+          </div>
+        </div>
+        <div className="gap-xs flex">
+          <Button kind="ghost" type="icon" size="medium">
+            ⚙️
+          </Button>
+          <Button kind="ghost" type="icon" size="medium">
+            🔔
+          </Button>
+          <Button kind="ghost" type="icon" size="medium">
+            ☰
+          </Button>
+        </div>
+      </>
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(B=(j=d.parameters)==null?void 0:j.docs)==null?void 0:B.source}}};var H,y,k;o.parameters={...o.parameters,docs:{...(H=o.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    variant: "desktop",
+    border: true,
+    children: <>
+        <div className="gap-xs flex items-center">
+          <Button kind="ghost" type="icon" size="medium">
+            ←
+          </Button>
+          <div className="gap-xs px-lg flex h-[40px] items-center rounded-md">
+            <span className="typography-headline-06 text-text">
+              Project Name
+            </span>
+          </div>
+          <Divider orientation="vertical" />
+          <HeaderButton>Files</HeaderButton>
+          <HeaderButton>Issues</HeaderButton>
+          <HeaderButton selected>Pull Requests</HeaderButton>
+          <HeaderButton>Actions</HeaderButton>
+        </div>
+        <div className="gap-xs flex items-center">
+          <Button kind="ghost" type="icon" size="medium">
+            ⚙️
+          </Button>
+          <Button kind="ghost" type="icon" size="medium">
+            🔔
+          </Button>
+        </div>
+      </>
+  }
+}`,...(k=(y=o.parameters)==null?void 0:y.docs)==null?void 0:k.source}}};var f,N,w;c.parameters={...c.parameters,docs:{...(f=c.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    variant: "desktop",
+    border: false,
+    children: <>
+        <div className="gap-xs flex items-center">
+          <div className="gap-xs px-lg flex h-[40px] items-center rounded-md">
+            <span className="typography-headline-06 text-text">
+              Simple Header
+            </span>
+          </div>
+        </div>
+        <div className="gap-xs flex items-center">
+          <HeaderButton>Login</HeaderButton>
+          <Button kind="primary" size="medium">
+            Sign Up
+          </Button>
+        </div>
+      </>
+  }
+}`,...(w=(N=c.parameters)==null?void 0:N.docs)==null?void 0:w.source}}};var D,b,z;l.parameters={...l.parameters,docs:{...(D=l.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    variant: "mobile",
+    border: true,
+    children: <>
+        <div className="flex items-center">
+          <div className="gap-sm p-lg flex h-auto w-[40px] items-center justify-center">
+            <div className="bg-text h-[16px] w-[18px] rounded-xs"></div>
+          </div>
+        </div>
+        <Button kind="ghost" type="icon" size="medium">
+          ☰
+        </Button>
+      </>
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1"
+    }
+  }
+}`,...(z=(b=l.parameters)==null?void 0:b.docs)==null?void 0:z.source}}};const U=["Playground","Desktop","Mobile","WithBorder","MinimalDesktop","MinimalMobile"];export{a as Desktop,c as MinimalDesktop,l as MinimalMobile,d as Mobile,r as Playground,o as WithBorder,U as __namedExportsOrder,q as default};
---
+++ assets/SwitcherItem.stories-Bpj8rSMi.js
@@ -0,0 +1,14 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{bE as n,bD as s,bC as l}from"./ContextMenuList-AIp8CmXM.js";import"./index-CY-HDqYb.js";import"./index-A6zbrgSY.js";import"./index-DWvd7WUe.js";import"./index-BDYMFBcC.js";import"./index-ZRCHFLd-.js";import"./sizes-BpUsT7VU.js";import"./colors-CoOIoG7W.js";const I={title:"WIP Components/Switcher/SwitcherItem",component:s,argTypes:{size:{control:"inline-radio",options:n},disabled:{control:"boolean"}}},e={args:{value:"a",children:"Item",size:"small",disabled:!1},render:r=>t.jsxs(l,{defaultValue:"a",children:[t.jsx(s,{...r}),t.jsx(s,{value:"b",size:r.size,disabled:r.disabled,children:"Other"})]})};var a,i,o;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    value: "a",
+    children: "Item",
+    size: "small",
+    disabled: false
+  },
+  render: args => <Switcher defaultValue="a">
+      <SwitcherItem {...args} />
+      <SwitcherItem value="b" size={args.size} disabled={args.disabled}>
+        Other
+      </SwitcherItem>
+    </Switcher>
+}`,...(o=(i=e.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};const g=["Playground"];export{e as Playground,g as __namedExportsOrder,I as default};
