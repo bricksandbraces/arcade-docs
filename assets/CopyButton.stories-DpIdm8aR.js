36397c67425f9266fc74db0af645bb4acf4d3196
hendriku <hendriku@users.noreply.github.com>
2026-04-20 15:37:06 +0000

    deploy: bricksandbraces/arcade-designsystem@0c5ef4e9964cb81835631bae8db493efd3753607

--- assets/CopyButton.stories-DpIdm8aR.js
+++
@@ -1,8 +0,0 @@
-import{i as n,E as i}from"./ContextMenuList-DPWyxZvf.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-CY-HDqYb.js";import{a as s}from"./index-B-lxVbXh.js";import"./index-A6zbrgSY.js";import"./index-DWvd7WUe.js";import"./index-BDYMFBcC.js";import"./index-ZRCHFLd-.js";import"./sizes-BpUsT7VU.js";import"./colors-CoOIoG7W.js";import"./v4-CtRu48qb.js";const f={title:"Components/CopyButton",component:i,argTypes:{size:{control:"inline-radio",options:n},disabled:{control:"boolean"},resetDelay:{control:"number"}},args:{value:"Hello World!",size:"large",disabled:!1,resetDelay:2e3}},o={args:{value:"This text will be copied to your clipboard",onCopy:a=>{s("onCopy")(a)}}};var e,r,t;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    value: "This text will be copied to your clipboard",
-    onCopy: (value: any) => {
-      action("onCopy")(value);
-    }
-  }
-}`,...(t=(r=o.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const x=["Playground"];export{o as Playground,x as __namedExportsOrder,f as default};
