ea6e9ce6b5140c16268e717c23c32b7693683d59
hendriku <hendriku@users.noreply.github.com>
2026-04-20 15:32:04 +0000

    deploy: bricksandbraces/arcade-designsystem@7562aad6f639ae38b97146a409b6081c3b36516b

---
+++ assets/EmptyState.stories-CBvMMlLn.js
@@ -0,0 +1,8 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{a2 as i}from"./ContextMenuList-DPWyxZvf.js";import"./index-CY-HDqYb.js";import"./index-A6zbrgSY.js";import"./index-DWvd7WUe.js";import"./index-BDYMFBcC.js";import"./index-ZRCHFLd-.js";import"./sizes-BpUsT7VU.js";import"./colors-CoOIoG7W.js";const h={title:"Components/EmptyState",component:i,argTypes:{orientation:{control:{type:"select"},options:["horizontal","vertical","center"]},href:{control:{type:"text"}}}},t=({text:a,...s})=>e.jsx("div",{children:e.jsx(i,{...s,children:a})});t.args={description:"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore.",title:"No search items found.",orientation:"horizontal",buttonLabel:"Create item",href:"#"};t.__docgenInfo={description:"",methods:[],displayName:"Playground"};t.__docgenInfo={description:"",methods:[],displayName:"Playground"};var o,r,n;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`({
+  text,
+  ...args
+}: React.ComponentProps<typeof EmptyState> & Record<string, any>) => {
+  return <div>
+      <EmptyState {...args}>{text}</EmptyState>
+    </div>;
+}`,...(n=(r=t.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const x=["Playground"];export{t as Playground,x as __namedExportsOrder,h as default};
