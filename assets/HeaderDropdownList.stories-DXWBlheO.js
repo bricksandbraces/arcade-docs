291b3977cc097207b18663fa6db05e637ad932b0
hendriku <hendriku@users.noreply.github.com>
2026-04-21 07:40:38 +0000

    deploy: bricksandbraces/arcade-designsystem@33ddb1d1dc0caccd75f9ae27b2d31615fd48c399

---
+++ assets/HeaderDropdownList.stories-DXWBlheO.js
@@ -0,0 +1,49 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{aD as g,aE as t}from"./ContextMenuList-5QEYwpib.js";import"./index-CY-HDqYb.js";import"./index-A6zbrgSY.js";import"./index-DWvd7WUe.js";import"./index-BDYMFBcC.js";import"./index-ZRCHFLd-.js";import"./sizes-BpUsT7VU.js";import"./colors-CoOIoG7W.js";const v={title:"WIP Components/Header/Dropdown/List",component:g,argTypes:{}},r={args:{children:e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"})]})}},d={args:{children:e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"})]})}},n={args:{children:e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"})]})}},s={args:{children:e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"}),e.jsx(t,{children:"Item Title"})]})}},o={args:{children:e.jsxs(e.Fragment,{children:[e.jsx(t,{children:"Enabled Item"}),e.jsx(t,{disabled:!0,children:"Disabled Item"}),e.jsx(t,{children:"Another Item"}),e.jsx(t,{children:"Final Item"})]})}};var i,m,a;r.parameters={...r.parameters,docs:{...(i=r.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+      </>
+  }
+}`,...(a=(m=r.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var I,l,p;d.parameters={...d.parameters,docs:{...(I=d.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+      </>
+  }
+}`,...(p=(l=d.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var c,D,w;n.parameters={...n.parameters,docs:{...(c=n.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+      </>
+  }
+}`,...(w=(D=n.parameters)==null?void 0:D.docs)==null?void 0:w.source}}};var H,L,h;s.parameters={...s.parameters,docs:{...(H=s.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Item Title</HeaderDropdownListItem>
+      </>
+  }
+}`,...(h=(L=s.parameters)==null?void 0:L.docs)==null?void 0:h.source}}};var T,x,j;o.parameters={...o.parameters,docs:{...(T=o.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    children: <>
+        <HeaderDropdownListItem>Enabled Item</HeaderDropdownListItem>
+        <HeaderDropdownListItem disabled>Disabled Item</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Another Item</HeaderDropdownListItem>
+        <HeaderDropdownListItem>Final Item</HeaderDropdownListItem>
+      </>
+  }
+}`,...(j=(x=o.parameters)==null?void 0:x.docs)==null?void 0:j.source}}};const A=["Playground","FourItems","FiveItems","SixItems","WithMixedStates"];export{n as FiveItems,d as FourItems,r as Playground,s as SixItems,o as WithMixedStates,A as __namedExportsOrder,v as default};
