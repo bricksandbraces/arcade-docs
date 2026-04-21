33847e20835e64325368459e51a369ed8587e324
hendriku <hendriku@users.noreply.github.com>
2026-04-21 13:00:09 +0000

    deploy: bricksandbraces/arcade-designsystem@057ce08b1a7f39fa4b6667de2d08bec208fbfcfe

---
+++ assets/Header.stories-CCvS2qci.js
@@ -0,0 +1,6 @@
+import{S as a}from"./StorybookHeader-CAxbvJHV.js";import"./jsx-runtime-D_zvdyIk.js";import"./ContextMenuList-DWuYW1Wj.js";import"./index-A6zbrgSY.js";import"./index-CY-HDqYb.js";import"./index-DWvd7WUe.js";import"./index-BDYMFBcC.js";const l={title:"Storybook/Header",component:a,argTypes:{title:{control:"text"},tag:{control:"text"}}},t={args:{title:"Button",tag:"v2.0.0"}};var r,o,e;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    title: "Button",
+    tag: "v2.0.0"
+  }
+}`,...(e=(o=t.parameters)==null?void 0:o.docs)==null?void 0:e.source}}};const g=["Header"];export{t as Header,g as __namedExportsOrder,l as default};
---
+++ assets/TableHeader.stories-DOgH8OI8.js
@@ -0,0 +1,15 @@
+import{E as u}from"./ContextMenuList-DWuYW1Wj.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-CY-HDqYb.js";import"./index-A6zbrgSY.js";import"./index-DWvd7WUe.js";import"./index-BDYMFBcC.js";const D={title:"WIP Components/DataTable/TableHeader",component:u,argTypes:{}},t={args:{title:"Data table",subtitle:"Subtitle or Description"}},e={args:{title:"Data table"}},a={args:{title:"Very long data table title that might wrap to multiple lines",subtitle:"This is a longer subtitle with more descriptive text about the table content"}};var r,o,s;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    title: "Data table",
+    subtitle: "Subtitle or Description"
+  }
+}`,...(s=(o=t.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var i,l,n;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    title: "Data table"
+  }
+}`,...(n=(l=e.parameters)==null?void 0:l.docs)==null?void 0:n.source}}};var p,c,m;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    title: "Very long data table title that might wrap to multiple lines",
+    subtitle: "This is a longer subtitle with more descriptive text about the table content"
+  }
+}`,...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const S=["Playground","TitleOnly","LongTitle"];export{a as LongTitle,t as Playground,e as TitleOnly,S as __namedExportsOrder,D as default};
