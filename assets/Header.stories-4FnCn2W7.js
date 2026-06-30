c17348dddd8a7215e7a5527c63d9a7c418c26df5
hendriku <hendriku@users.noreply.github.com>
2026-06-30 15:35:52 +0000

    deploy: bricksandbraces/arcade-designsystem@0d874c9bd580cb0d090e06f83234e1a780a5ef2d

---
+++ assets/Header.stories-4FnCn2W7.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{n as t,t as n}from"./storybook-CCDVfGSd.js";var r,i,a,o;e((()=>{n(),r={title:`Storybook Helper/Header`,component:t,tags:[`autodocs`],argTypes:{title:{control:`text`},tag:{control:`text`}}},i={args:{title:`Button`,tag:`v2.0.0`}},a=i,i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    title: "Button",
+    tag: "v2.0.0"
+  }
+}`,...i.parameters?.docs?.source}}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`Header`,...a.parameters?.docs?.source}}},o=[`Header`,`Playground`]}))();export{i as Header,a as Playground,o as __namedExportsOrder,r as default};
