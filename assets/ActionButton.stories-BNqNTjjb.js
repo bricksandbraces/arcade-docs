8b9f33b199305a0402f40af7aed0c050b7562fe8
hendriku <hendriku@users.noreply.github.com>
2026-09-09 05:50:12 +0000

    deploy: bricksandbraces/arcade-designsystem@08c6a5ad53b381770613c73fb74cf18baab89f12

---
+++ assets/ActionButton.stories-BNqNTjjb.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{Ai as t,Li as n,Mi as r,Ra as i,Si as a,ci as o,sa as s,t as c,xo as l,za as u}from"./iframe-DZC8GE3v.js";var d,f,p,m,h;e((()=>{c(),a(),d=l(),f={component:o,title:`Buttons & Actions/ActionButton`,tags:[`autodocs`],args:{label:`Actions`,size:`medium`,variant:`rounded`,disabled:!1,loading:!1,title:`Actions`,items:[{label:`Edit`,icon:s,onClick:()=>void 0},{label:`Rotate`,icon:n,onClick:()=>void 0},{label:`Upload`,icon:t,disabled:!0,onClick:()=>void 0},{label:`Delete`,danger:!0,showDivider:!0,icon:r,onClick:()=>void 0}]},argTypes:{size:{control:{type:`inline-radio`},options:i},variant:{control:{type:`inline-radio`},options:u},IconLeft:{table:{disable:!0}},IconRight:{table:{disable:!0}},items:{table:{disable:!0}}},render:e=>(0,d.jsx)(o,{...e}),parameters:{docs:{description:{component:`Split button combining a primary action with a menu of secondary actions. Use when one action is the obvious default but related options should stay one click away.`}}}},p={},m={args:{label:void 0,IconLeft:s,tooltipContent:`Open actions`,tooltipPlacement:`bottom`}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: undefined,
+    IconLeft: IconEdit,
+    tooltipContent: "Open actions",
+    tooltipPlacement: "bottom"
+  }
+}`,...m.parameters?.docs?.source}}},h=[`Playground`,`IconOnly`]}))();export{m as IconOnly,p as Playground,h as __namedExportsOrder,f as default};
