c4bd288395e2fd1eaced3ab5acb4ff9aa77442e0
hendriku <hendriku@users.noreply.github.com>
2026-09-15 14:03:38 +0000

    deploy: bricksandbraces/arcade-designsystem@afbe3c64a69359ddf4b7d6d8324fc90d466eab88

---
+++ assets/EventCalendarTimeRow.stories-B400YznN.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-jRWAZmH_.js";import{bn as t,os as n,t as r,xs as i}from"./iframe-CVgNtwa1.js";import{n as a,t as o}from"./figmaDesign-CO6i5n9C.js";import{a as s,i as c,n as l,t as u}from"./figmaFrame-thsBqZpU.js";var d,f,p,m,h;e((()=>{r(),i(),a(),s(),l(),d=n(),f={title:`Display & Data/EventCalendar/EventCalendarTimeRow`,component:t,tags:[`autodocs`],parameters:{design:o(c(`5578:16322`))},args:{time:new Date(2026,8,4,16,8),columns:7,todayColumn:3},render:e=>(0,d.jsx)(u,{node:`5578:16322`,width:1e3,height:32,children:(0,d.jsx)(t,{...e})})},p={},m={args:{columns:1,todayColumn:0}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{}`,...p.parameters?.docs?.source}}},m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    columns: 1,
+    todayColumn: 0
+  }
+}`,...m.parameters?.docs?.source}}},h=[`Week`,`Day`]}))();export{m as Day,p as Week,h as __namedExportsOrder,f as default};
